f32e51b3e4d8cc4b83d4b6cbfd5438e797873c67
71060 <71060@LAPTOP-GEF01K5S>
2024-03-09 18:03:27 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    int n;
+    int x;
+    int count = 0;
+
+    srand(time(0));
+    n = rand() % 100 + 1;
+
+    cout
+        << "The number is 1-100" << endl
+        << "Guess the number for 20 try" << endl;
+
+    do
+    {
+        cout << "\nInput  number\n\t-> ";
+        cin >> x;
+        if (x == n) cout << "You god damn right";{
+         count++;}
+          if ( x>n) {
+              cout << "Number is lower";
+        }
+          if (x < n) {
+              cout << "Number is higher";
+          }
+    } while (x != n && count < 20);
+
+
+    return 0;
+}
